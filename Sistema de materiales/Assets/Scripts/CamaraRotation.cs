923a57ad3d4d7e9039b6b7329a62035fd0503459
Sireth6 <fsvg06@gmail.com>
2023-03-12 15:21:06 -0600

    Avance Sistema de materiales
    Se agregaron botones para cambio de texturas

---
+++ Sistema de materiales/Assets/Scripts/CamaraRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraRotation : MonoBehaviour
+{
+    public float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetMouseButton(0))
+        {
+            transform.eulerAngles += speed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+
+
+        }
+    }
+}
---
+++ Sistema de materiales/Assets/Scripts/GrabRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabRotation : MonoBehaviour
+{
+    float rotationSpeed = 3f;
+
+    void OnMouseDrag()
+    {
+        float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        transform.Rotate(Vector3.down, XaxisRotation, Space.World);
+        transform.Rotate(Vector3.right, YaxisRotation, Space.World);
+    }
+
+    private void OnMouseUp()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+}
